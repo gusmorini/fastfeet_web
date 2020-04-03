2fa450ff9f9e5a76d7afacdcf124c36bca34b4eb
Gustavo Morini <gustavommh@gmail.com>
2020-04-03 13:33:36 -0300

    create AddButton

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { darken } from "polished";
+import { FaPlus } from "react-icons/fa";
 
 export default function Button(props) {
   return <FastButton {...props}>{props.children}</FastButton>;
+}
+
+export function AddButton(props) {
+  return (
+    <FastButton {...props}>
+      <FaPlus />
+      {props.children}
+    </FastButton>
+  );
 }
 
 const FastButton = styled(Link)`
@@ -15,6 +25,15 @@ const FastButton = styled(Link)`
   font-weight: bold;
   padding: 10px 20px;
   transition: 0.5s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+
+  svg {
+    margin-right: 5px;
+    font-size: 10px;
+  }
 
   &:hover {
     background: ${darken(0.05, "#7d40e7")};
