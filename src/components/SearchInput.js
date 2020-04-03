8597854eae30a3d60eb7227281979e277d6684ef
Gustavo Morini <gustavommh@gmail.com>
2020-04-03 13:33:47 -0300

    fix SearchInput

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,14 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import { FaSearch } from "react-icons/fa";
 
-export default function SearchInput(props) {
-  return <Search {...props}>{props.children}</Search>;
-}
-
-const Search = styled.input`
+const Box = styled.div`
   font-size: 14px;
   color: #999999;
   border: 1px solid #dddddd;
   border-radius: 4px;
-  padding: 0 20px;
+  width: 100%;
+  max-width: 237px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 0px 10px;
+
+  input {
+    background: none;
+    border: none;
+    color: #999999;
+    padding-left: 10px;
+    width: 100%;
+  }
 `;
+
+export default function SearchInput(props) {
+  return (
+    <Box>
+      <FaSearch />
+      <input {...props}>{props.children}</input>
+    </Box>
+  );
+}
