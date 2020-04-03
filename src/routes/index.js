b5119949018dbb3df663d1d907e3fc4a0aec4fc4
Gustavo Morini <gustavommh@gmail.com>
2020-04-03 13:00:11 -0300

    create page Recipient

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import Route from "./Route";
 import SignIn from "../pages/SignIn";
 import Orders from "../pages/Orders";
 import Deliveryman from "../pages/Deliveryman";
+import Recipient from "../pages/Recipient";
 
 export default function Routes() {
   return (
@@ -13,6 +14,7 @@ export default function Routes() {
       <Route path="/" exact component={SignIn}></Route>
       <Route path="/orders" component={Orders} isPrivate></Route>
       <Route path="/deliveryman" component={Deliveryman} isPrivate></Route>
+      <Route path="/recipient" component={Recipient} isPrivate></Route>
       {/* rota 404 */}
       <Route path="/" component={() => <h1>404</h1>}></Route>
     </Switch>
