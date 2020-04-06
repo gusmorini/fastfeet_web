0ee506af707c399beabe1e5bf46fe2c1210edf07
Gustavo Morini <gustavommh@gmail.com>
2020-04-06 20:47:07 -0300

    style deliveryman_add

--- src/pages/Deliveryman/DeliverymanAdd/index.js
+++ src/pages/Deliveryman/DeliverymanAdd/index.js
@@ -1,5 +1,29 @@
 import React from "react";
 
+import { SubMenu, BoxButton } from "../../../components/SubMenu";
+import Title from "../../../components/Title";
+import { SaveButton, ReturnButton } from "../../../components/Button";
+
+import { BoxForm } from "../../../components/Form";
+
 export default function DeliverymanAdd() {
-  return <h1>Adicionar entregador</h1>;
+  return (
+    <>
+      <SubMenu>
+        <Title>Cadastro de entregadores</Title>
+        <BoxButton>
+          <ReturnButton>voltar</ReturnButton>
+          <SaveButton>SALVAR</SaveButton>
+        </BoxButton>
+      </SubMenu>
+
+      <BoxForm>
+        {/* adicionar input file */}
+        <label htmlFor="">Nome</label>
+        <input type="text" placeholder="nome completo" />
+        <label htmlFor="">E-mail</label>
+        <input type="text" placeholder="exemple@fastfeet.com" />
+      </BoxForm>
+    </>
+  );
 }
