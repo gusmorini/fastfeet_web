b9d6ba3c47cfac10c282d707f519d8724deb137d
Gustavo Morini <gustavommh@gmail.com>
2020-04-06 20:04:08 -0300

    create DeliverymanAdd route deliveryman_add

---
+++ src/pages/Deliveryman/DeliverymanAdd/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DeliverymanAdd() {
+  return <h1>Adicionar entregador</h1>;
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,10 @@ import Route from "./Route";
 
 import SignIn from "../pages/SignIn";
 import Orders from "../pages/Orders";
+
 import Deliveryman from "../pages/Deliveryman";
+import DeliverymanAdd from "../pages/Deliveryman/DeliverymanAdd";
+
 import Recipient from "../pages/Recipient";
 import Problems from "../pages/Problems";
 
@@ -13,10 +16,20 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn}></Route>
+
       <Route path="/orders" component={Orders} isPrivate></Route>
+
       <Route path="/deliveryman" component={Deliveryman} isPrivate></Route>
+      <Route
+        path="/deliveryman_add"
+        component={DeliverymanAdd}
+        isPrivate
+      ></Route>
+
       <Route path="/recipient" component={Recipient} isPrivate></Route>
+
       <Route path="/problems" component={Problems} isPrivate></Route>
+
       {/* rota 404 */}
       <Route path="/" component={() => <h1>404</h1>}></Route>
     </Switch>
